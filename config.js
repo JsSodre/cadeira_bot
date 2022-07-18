ada5d53f3e7f42fa56f837aa0022bc6baafcf2ce
JsSodre <100088598+JsSodre@users.noreply.github.com>
2022-07-18 17:45:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'D2p3UjDgE8IGVMwZX5bJlJGXT',
-    consumer_secret:      'eQJfXbl3nnlc854RQhtfNFD5iy8xeUL7tkW5i6zYWWUe01iLI8',
-    access_token:         '1496522615053299712-KvdezamzBXnlxTKSfX8MYLdSKb2NMf',
-    access_token_secret:  'rnO4jgN5XdbLrtyEe0TsNM0tEz28mX1tAhZ47fnjBVGWC'
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
   }
